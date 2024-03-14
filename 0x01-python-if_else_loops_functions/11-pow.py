60e7d66ef2fb0a60de4030545edc7b50e46cf00a
Yann Adjumany <yvay03@gmail.com>
2024-03-13 19:59:17 -0700

    11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    result = 1
-    for _ in range(b):
-        result *= a
-    return result
+    if b == 0:
+        return 1
+    elif b > 0:
+        result = 1
+        for _ in range(b):
+            result *= a
+        return result
+    else:  # b < 0
+        result = 1
+        for _ in range(-b):
+            result *= a
+        return 1 / result
