bb68f1df30217cfcc7d80e34ac317f54dd618954
Yann Adjumany <yvay03@gmail.com>
2024-03-16 04:53:49 -0700

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 
-print(''.join([chr(_) if _ % 2 == 0 else chr(_ - 32) for _ in range(122, 96, -1)]))
+print("{}".format(''.join([chr(_) if _ % 2 == 0 else chr(_ - 32) for _ in range(122, 96, -1)])))
