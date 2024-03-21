249f0020c2b6994775e815c50fac4b18223a25b4
Yann Adjumany <yvay03@gmail.com>
2024-03-20 18:47:22 -0700

    44

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     import hidden_4
 
     names = dir(hidden_4)
-    for name  in names:
+    for name in names:
         if not name.startswith('__'):
             print(name)
