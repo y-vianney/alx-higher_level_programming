271281e6c014e07d29d4a690a14a56aaf9df5d16
Yann Adjumany <yvay03@gmail.com>
2024-03-22 19:50:47 -0700

    9

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+    """
+    Delete a key in a dictionary.
+    """
+
+    if key in a_dictionary.keys():
+        del a_dictionary[key]
+
+    return a_dictionary
