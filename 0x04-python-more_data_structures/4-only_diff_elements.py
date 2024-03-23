39b8bd8e4e5f126c350b7bab5e6f269bc1526eef
Yann Adjumany <yvay03@gmail.com>
2024-03-22 19:38:10 -0700

    5

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -5,4 +5,4 @@ def only_diff_elements(set_1, set_2):
     Return a set of all elements present in only one set.
     """
 
-    return set(_ for _ in set_1 if _ not in set_2)
+    return set_1.symmetric_difference(set_2)
