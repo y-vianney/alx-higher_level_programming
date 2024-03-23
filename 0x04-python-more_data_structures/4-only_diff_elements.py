af3d3b6ae4213c2decbe34ab4d0d1eae3c934584
Yann Adjumany <yvay03@gmail.com>
2024-03-22 19:35:42 -0700

    5

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    """
+    Return a set of all elements present in only one set.
+    """
+
+    return sorted(set(_ for _ in set_1 if _ not in set_2))
