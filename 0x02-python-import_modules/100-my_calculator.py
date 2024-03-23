ab9f30f2f62d8661c8d10b13c497d46f4751cf8a
Yann Adjumany <yvay03@gmail.com>
2024-03-23 06:03:56 -0700

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     args = sys.argv[1:]
 
     if len(args) == 3:
-        if args[1] not in "-+/*":
+        if args[1] not in "+-*/":
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
         else:
@@ -25,4 +25,4 @@ if __name__ == "__main__":
                 print("{} {} {} = {}".format(a, args[1], b, div(a, b)))
     else:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1);
+        sys.exit(1)
