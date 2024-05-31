3c4212bb20fdb44cd9ed476ca982fe390b79204e
Yann Adjumany <yvay03@gmail.com>
2024-05-31 05:39:36 -0700

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        for elt in my_list:
-            print(elt)
-        print('\n')
-    except IndexError:
-        print('')
+    index = 0
 
+    while True:
+        try:
+            print(elt[index])
+            index += 1
+            if (index + 1 == x):
+                break
+        except IndexError:
+            print('')
+
+    return index + 1
