73cfc290827df176ae845df211238cbb84c89c5e
Yann Adjumany <yvay03@gmail.com>
2025-03-11 03:17:19 -0700

    pycodestyle

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,8 +6,9 @@ import sys
 def safe_print_integer_err(value):
     """
     Function that prints an integer
-    Returns True if value has been correctly printed (it means the value is an integer)
-    Otherwise, returns False and prints in stderr the error precede by Exception:
+    Returns True if value has been correctly printed
+    Otherwise, returns False and prints in stderr
+    the error precede by Exception:
     """
     output = True
 
