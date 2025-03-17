de86d009b0cd3161ff19cc9cb3b2d332d9b3b8cb
Yann A. V. ADJUMANY <69991917+y-vianney@users.noreply.github.com>
2025-03-17 16:24:47 +0100

    bytecode -> python

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import dis
+
+
+def magic_calculation(a, b):
+	"""Does magic calculation based on Py bytecode"""
+	result = 0
+
+
+	for i in range(1, 3):
+		try:
+			if i > a:
+				raise Exception('Too Far')
+
+				result += a**b / i
+		except Exception as e:
+			result = a + b
+			break
+
+	return result
+
+
+print(dis.dis(magic_calculation))
