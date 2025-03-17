4cfd4d924052156fcd6e4b1ee82793c4c76d9ce1
Yann Adjumany <yvay03@gmail.com>
2025-03-17 09:12:22 -0700

    magic calculation

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-	"""Does magic calculation based on Py bytecode"""
-	result = 0
+    """Does magic calculation based on Py bytecode"""
+    result = 0
 
 
-	for i in range(1, 3):
-		try:
-			if i > a:
-				raise Exception('Too Far')
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too Far')
 
-				result += a**b / i
-		except Exception as e:
-			result = a + b
-			break
+                result += a**b / i
+        except Exception as e:
+            result = a + b
+            break
 
-	return result
+    return result
